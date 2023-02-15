288f231dd917e853e3bb02da7b56e95cd8c7ab8c
Edward Marzal <edsanchezmarzal@gmail.com>
2023-02-15 16:57:43 -0500

    navigation cases

---
+++ features/common/modules/Navigation/navigationConsts.js
@@ -0,0 +1,7 @@
+import { HiPhoneArrowUpRight } from "react-icons/hi2";
+
+export const navigationLinks = [
+    {title: 'Properties', link:'/properties'},
+    {title: 'Contact', link:'/contact'},
+    {title: '(44) 555 555', link:'tel:(44) 555 555', icon: <HiPhoneArrowUpRight/>},
+]
