f54619427893f4f7555cd9d76cf5acae907d0269
Edward Marzal <edsanchezmarzal@gmail.com>
2023-02-15 16:32:10 -0500

    testing

--- features/common/modules/Navigation/components/NavigationDesktop/NavigationDesktop.js
+++ features/common/modules/Navigation/components/NavigationDesktop/NavigationDesktop.js
@@ -1,5 +1,14 @@
+import { Box } from "@chakra-ui/react";
+
 const NavigationDesktop = () => {
-    return <>NavigationDesktop</>;
+    return (<Box 
+        color='blue.600' 
+        paddingY='2rem' 
+        backgroundColor='white' 
+        display={{base:"none", md:'block'}}
+        > 
+            TEST
+        </Box>);
 }
  
 export default NavigationDesktop;
