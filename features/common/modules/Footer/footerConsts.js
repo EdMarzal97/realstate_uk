1e13ebd8bb8947dad956949cc4ab605a428bcbab
Edward Marzal <edsanchezmarzal@gmail.com>
2023-02-21 11:16:26 -0500

    started with footer

---
+++ features/common/modules/Footer/footerConsts.js
@@ -0,0 +1,30 @@
+export const services = [
+    { name: "Our Services", link: "/our-services" },
+    { name: "Meet The Team", link: "/meet-the-team" },
+    { name: "Careers at WTL", link: "/careers-at-wtl" },
+    { name: "Latest News & Videos", link: "/latest-news" }
+  ];
+  
+  export const about = [
+    { name: "Buy", link: "/buy" },
+    { name: "Rent", link: "/rent" },
+    { name: "Commercial", link: "/commercial" },
+    { name: "Mortgage Services", link: "/mortgage-services" },
+    { name: "Property Management", link: "/property-management" }
+  ];
+  
+  export const workWithUs = [
+    { name: "Off-Plan", link: "/off-plan" },
+    { name: "Holiday Homes", link: "/holiday-homes" },
+    { name: "Home Maintenance", link: "/home-maintenance" },
+    { name: "Sell with us", link: "/sell-with-us" },
+    { name: "Let with us", link: "/let-with-us" }
+  ];
+  
+  export const ourOffices = [
+    { name: "Birmingham", link: "/office/birmingham" },
+    { name: "London", link: "/office/london" },
+    { name: "York", link: "/office/york" },
+    { name: "Manchester", link: "/office/manchester" },
+    { name: "Liverpool", link: "/office/liverpool" }
+  ];
