b0a16b0f6292d3baa94d9e3c17afb6edbafba0f4
Edward Marzal <edsanchezmarzal@gmail.com>
2023-02-22 21:32:02 -0500

    creating footer

---
+++ features/common/modules/Footer/Footer.js
@@ -0,0 +1,23 @@
+import { Box, Text } from '@chakra-ui/react';
+import { services, about, ourOffices, workWithUs } from './footerConsts'
+
+const Footer = () => {
+    return ( 
+        <Box backgroundColor='blue.600' >
+            <Box maxWidth='1280px' margin='0 auto' paddingY='3rem' paddingX={{ base: '2rem', sm: '0' }}>
+                
+            </Box>
+        </Box>
+     );
+}
+ 
+export default Footer;
+
+const FooterLink = ({ name, link }) => {
+    return ( 
+        <Text>
+
+        </Text>
+     );
+}
+ 
