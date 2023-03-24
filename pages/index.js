0eaa98ae3207891137ef11b6bd8b68598c2fe36f
Edward Marzal <edsanchezmarzal@gmail.com>
2023-03-24 02:50:31 -0500

    cleaning

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,3 @@
-import Footer from "@/features/common/modules/Footer/Footer";
-import Navigation from "@/features/common/modules/Navigation";
 import DefaultLayout from "@/features/Layouts/DefaultLayouts/DefaultLayout";
 
 export default function Home() {
